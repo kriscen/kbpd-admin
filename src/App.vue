637d7d8c5ddf0bb43efa0c25546b26a671dcebbc
Kris <kriscen@163.com>
2025-05-22 09:31:28 +0800

    feat(*): project init
    project init

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <router-view :key="key" />
+  <h1>app</h1>
 </template>
 
+
 <script setup lang="ts">
-import { isMobile, watchResize } from '@bassist/utils'
 
-const route = useRoute()
-const key = computed(() => `${String(route.name || route.path)}-${new Date()}`)
 
-watchResize(() => {
-  document.body.className = `platform-${isMobile() ? 'mobile' : 'desktop'}`
-})
+
+
 </script>
+
+
+<style scoped lang="less">
+
+
+
+
+</style>
