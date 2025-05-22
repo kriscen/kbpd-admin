d9f8943f51cfb602623c5aca45071b310ab16a1a
Kris <kriscen@163.com>
2025-05-22 15:27:06 +0800

    feat(*): project init
    别名设置

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,5 +15,10 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()]
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve("./src") // 相对路径别名配置，使用 @ 代替 src
+    }
+  }
 });
